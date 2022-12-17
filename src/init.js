b28b8e941225f6bb2548da59ca607e8297f6aa33
bibhasingh <bibhaanil@gmail.com>
2022-12-17 10:54:46 +0100

    modified filename

--- src/init.js
+++ src/init.js
@@ -12,4 +12,4 @@ import getTimeComponent from "./components/time.js";
 // SHOW 00:00:000 BY DEFAULT
 const timeElement = document.getElementById("time");
 const timeComponent = getTimeComponent(data);
-timeElement.append(timeComponent);
+timeElement.append(timeComponent);                                      
